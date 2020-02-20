a5f3420514b71dc5df174f51d960769179dfa4df
Jakub Slouk <jakub.slouk@i.cz>
2020-02-20 19:33:06 +0100

    story time

--- src/com/Engeto/Main.java
+++ src/com/Engeto/Main.java
@@ -1,8 +1,18 @@
 package com.Engeto;
+import java.util.Scanner; // načítání vstupu z klávesnice pomocí konzole
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("I will tell you a story, but I need some information first.\n" + "What is the main character called?:");
+        String name = scanner.nextLine();
+        System.out.println("What is their job?");
+        String job = scanner.nextLine();
+        System.out.println("Here is the story: \n" +
+                "Once upon a time there was "+name+", who was a "+job+".\n" +
+                "On the way to work,"+name+" reflected on life.\n" +
+                "Perhaps "+name+" will not be a "+job+" forever.");
     }
 }
